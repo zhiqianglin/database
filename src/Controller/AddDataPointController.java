24ddc1a216ff7fcce2791115c42ef467b69705e4
zhiqianglin <limcc321@gmail.com>
2017-04-13 23:56:55 -0400

    Add Annoying TimePicker

--- src/Controller/AddDataPointController.java
+++ src/Controller/AddDataPointController.java
@@ -1,10 +1,17 @@
 package Controller;
 
+import com.jfoenix.controls.JFXTimePicker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.jfoenix.controls.JFXDatePicker;
+
 
 /**
  * Created by zlin on 4/13/17.
@@ -14,6 +21,8 @@ public class AddDataPointController {
     public TextField dataValue;
     public ChoiceBox locationNames;
     public ChoiceBox dataType;
+    public DatePicker date;
+    public JFXTimePicker time;
 
 
     public void initialize() {
@@ -24,6 +33,11 @@ public class AddDataPointController {
         //get the DataType Dynamically
         dataType.getItems().addAll("Mold", "Whatever");
         dataType.getSelectionModel().selectFirst();
+
+        date.setValue(LocalDate.now());
+        time.setValue(LocalTime.now());
+//        checkInDatePicker.setValue(LocalDate.now());
+
     }
 
     public void submit(ActionEvent actionEvent) {
@@ -34,11 +48,11 @@ public class AddDataPointController {
             Helper.showAlert("Wrong value", "Please enter valid data value");
             return;
         }
-
+        System.out.println(date.getClass());
         System.out.println(locationNames.getValue());
         System.out.println(dataType.getValue());
         System.out.println(dataValue.getText());
-
+        System.out.println(time.getValue());
         //write to database
 
 
