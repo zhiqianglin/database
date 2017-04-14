8daf71696ecf8c2cf526a184b57b9cae164bae11
zhiqianglin <limcc321@gmail.com>
2017-04-14 16:54:52 -0400

    Edit Controllers for fxmls

---
+++ src/Controller/PendingCityOfficial.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by zlin on 4/14/17.
+ */
+public class PendingCityOfficial {
+    public void back(ActionEvent actionEvent) {
+    }
+
+    public void reject(ActionEvent actionEvent) {
+    }
+
+    public void accpet(ActionEvent actionEvent) {
+    }
+}
