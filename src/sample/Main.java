3edd88edcfe9c90aeedffc4723deace4378ad6b9
wycgatech <wycgatech@gmail.com>
2017-04-15 16:32:36 -0400

    register

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 //        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("fxml/registration.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 400, 600));
         primaryStage.show();
     }
 
